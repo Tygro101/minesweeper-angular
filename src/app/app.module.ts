f444c3a9a1cfb78680787ac45d46765480c7ddca
Ran <tayebran@gmail.com>
2017-08-29 10:24:25 +0000

    remove unnecessary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponentTop } from './app.component.top';
 import { AppComponent } from './app.component';
@@ -13,8 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponentTop]
